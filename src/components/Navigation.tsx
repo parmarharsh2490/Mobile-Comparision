c1ace8499d6c89b6a4808bf6c6eb99d9a9e4f6ee
parmarharsh2490 <2101031000125@silveroakuni.ac.in>
2024-03-23 19:51:09 +0530

    'responsive'

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,33 +4,37 @@ import { Button } from './ui/button'
 import { CgProfile } from "react-icons/cg";
 import { Link } from 'react-router-dom';
 import { MobileContext } from './store';
+
 const Navigation = () => {
-    // const {isLogin} = useContext(MobileContext);
-    const  {user} = useContext(MobileContext);
-    console.log('user' + user?.username);
-  return (
-    <nav className='mb-2'>
-      <ul className='flex flex-row items-center justify-start px-7  relative mt-2'>
-        <Link to={'/home'}> <img src={logo} alt="" height={'100px'} width={'200px'}/></Link>
-        <Link to={'/home'} className='relative float-left left-[350px] border border-b-black border-t-white border-x-white'>Home</Link>
-        <Link to={'/phonesList'} className='relative float-left left-[450px] border border-b-black border-t-white border-x-white'>Phones</Link>
-        <Link to={'/wishlist'} className='relative float-left left-[550px] border border-b-black border-t-white border-x-white'>WishList</Link>
-        {
-            user?.username === ''  ? (
-                <>
-                <Link to={'/signup'} className=' absolute top-0 right-1'><Button>Signup</Button></Link>
-        <Link to={'/login'} className=' absolute top-0 right-24' ><Button>Login</Button></Link>
-                </>
-            )
-            :
-            (<li className='absolute top-0 right-24 flex mt-2 border border-b-black border-t-white border-x-white'>
-            <p className='m-1'><CgProfile size={'1.4rem'} /></p>
-            <p className='mx-1 font-semibold text-1xl m-[3px]'>Hello ,  {user?.username}</p>
-        </li>)
-        }
-      </ul>
-    </nav>
-  )
+    const { user } = useContext(MobileContext);
+
+    return (
+        <nav className='mb-2'>
+            <div className='flex flex-row items-center justify-between px-7 relative mt-2'>
+                <Link to={'/home'} className="flex items-center">
+                    <img src={logo} alt="" className="h-12 w-auto" />
+                </Link>
+                <div className="flex hidden items-center md:block lg:block  md:space-x-16  lg:space-x-32 sm:hidden ">
+                    <Link to={'/home'} className='nav-link'>Home</Link>
+                    <Link to={'/phonesList'} className='nav-link'>Phones</Link>
+                    <Link to={'/wishlist'} className='nav-link'>WishList</Link>
+                </div>
+                <div className="flex items-center space-x-4">
+                    {user?.username === '' ? (
+                        <>
+                            <Link to={'/signup'}><Button>Signup</Button></Link>
+                            <Link to={'/login'}><Button>Login</Button></Link>
+                        </>
+                    ) : (
+                        <Link to={'/profile'} className='flex items-center'>
+                            <CgProfile size={'1.4rem'} />
+                            <p className='font-semibold text-1xl ml-1'>Hello, {user?.username}</p>
+                        </Link>
+                    )}
+                </div>
+            </div>
+        </nav>
+    );
 }
 
-export default Navigation
+export default Navigation;
